2ad3b373e30d35f0e04f17445ec4d8527328142e
Abiel Zulio M <abielzm@gmail.com>
2022-12-17 16:29:36 +0700

    fix: prettier

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<div />
-	</React.StrictMode>
+  <React.StrictMode>
+    <div />
+  </React.StrictMode>
 );
