c601d80d3a4cf7f2663950d2a36189567392235f
Abiel Zulio M <abielzm@gmail.com>
2022-12-17 16:24:18 +0700

    fix: clean up FE

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<div />
+	</React.StrictMode>
 );
