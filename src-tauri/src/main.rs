b4365313bb10b893a8c8fdd4781d931489205f4c
Abiel Zulio M <abielzm@gmail.com>
2022-12-18 17:40:46 +0700

    feat: custom window menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,14 +4,84 @@
 )]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+
+use tauri::{api, CustomMenuItem, Menu, MenuItem, Submenu, AboutMetadata};
+use tauri::{Manager};
+
+pub fn custom_window_menu() -> Menu {
+  let app_name: &str = "Supabase";
+  let mut menu = Menu::new();
+
+  #[cfg(target_os = "macos")]
+  {
+    menu = menu.add_submenu(Submenu::new(
+      app_name,
+      Menu::new()
+        .add_native_item(MenuItem::About(
+          app_name.to_string(),
+          AboutMetadata::default(),
+        ))
+        .add_native_item(MenuItem::Separator)
+        .add_native_item(MenuItem::Services)
+        .add_native_item(MenuItem::Separator)
+        .add_native_item(MenuItem::Hide)
+        .add_native_item(MenuItem::HideOthers)
+        .add_native_item(MenuItem::ShowAll)
+        .add_native_item(MenuItem::Separator)
+        .add_native_item(MenuItem::Quit),
+    ));
+  }
+
+  #[cfg(not(target_os = "macos"))]
+  {
+    menu = menu.add_submenu(Submenu::new(
+      "File",
+      Menu::new().add_native_item(MenuItem::Quit),
+    ));
+  }
+
+  let mut window_menu = Menu::new();
+  window_menu = window_menu.add_native_item(MenuItem::Minimize);
+  #[cfg(target_os = "macos")]
+  {
+    window_menu = window_menu.add_native_item(MenuItem::Zoom);
+    window_menu = window_menu.add_native_item(MenuItem::Separator);
+    window_menu = window_menu.add_native_item(MenuItem::EnterFullScreen);
+    window_menu = window_menu.add_native_item(MenuItem::Separator);
+  }
+  window_menu = window_menu.add_native_item(MenuItem::CloseWindow);
+  menu = menu.add_submenu(Submenu::new("Window", window_menu));
+
+  let help_menu = Menu::new();
+  let github = CustomMenuItem::new("github".to_string(), "View Source Code");
+  let twitter = CustomMenuItem::new("twitter".to_string(), "Author on Twitter");
+  menu = menu.add_submenu(Submenu::new("About", help_menu.add_item(github).add_item(twitter)));
+
+  menu
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .menu(custom_window_menu())
+    .on_menu_event(|event| {
+      match event.menu_item_id() {
+        "github" => {
+          api::shell::open(
+            &event.window().get_window("main").unwrap().shell_scope(),
+            "https://github.com/abielzulio/supabase-desktop".to_string(),
+            None,
+          ).unwrap()
+        }
+        "twitter" => {
+          api::shell::open(
+            &event.window().get_window("main").unwrap().shell_scope(),
+            "https://twitter.com/abielzulio".to_string(),
+            None,
+          ).unwrap()
+        }
+        _ => {}
+      }
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
